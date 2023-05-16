be9839ad574f2f6b237eeb6306c52cf3cd5975aa
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-16 17:59:54 +0600

    added to env local file

--- src/Providers/AuthPrividers.jsx
+++ src/Providers/AuthPrividers.jsx
@@ -7,10 +7,12 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthPrividers = ({ children }) => {
-const [user,setUser] = useState(null)
+    const [user, setUser] = useState(null)
+    const [loading, setLoding] = useState(true)
 
     const authInfo = {
         user,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAFYd_Tya7WKxvhG4pbhMO21CHnntSDxiA",
-  authDomain: "cars-doctor-41533.firebaseapp.com",
-  projectId: "cars-doctor-41533",
-  storageBucket: "cars-doctor-41533.appspot.com",
-  messagingSenderId: "1085922764018",
-  appId: "1:1085922764018:web:d8c854e3e8c9e3288ddc6b"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
